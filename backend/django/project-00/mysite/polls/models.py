5b60860f4003b7c3f866ffed1c5999a597ebf81f
Pedro Salomon <pedro.salomon@gmail.com>
2026-06-09 20:31:17 +0200

    testing

--- backend/django/project-00/mysite/polls/models.py
+++ backend/django/project-00/mysite/polls/models.py
@@ -1,4 +1,5 @@
-from datetime import datetime, timezone
+import datetime
+from django.utils import timezone
 
 from django.db import models
 
@@ -9,11 +10,11 @@ class Question(models.Model):
 
     def __str__(self):
         return self.question_text
-    
+
     def was_published_recently(self):
-        return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
-    
-    
+        now = timezone.now()
+        # return self.pub_date >= now - datetime.timedelta(days=1)
+        return now - datetime.timedelta(days=1) <= self.pub_date <= now
 
 
 class Choice(models.Model):
